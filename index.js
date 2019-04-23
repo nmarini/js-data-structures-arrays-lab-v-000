17389a908a08166f610512874d2f202953c6aa43
Nicolas Marini <nmarini44@gmail.com>
2019-04-23 00:28:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,3 +15,8 @@ function destructivelyRemoveLastDriver() {
 function destructivelyRemoveFirstDriver() {
   drivers.shift()
 }
+
+function appendDriver(name) {
+  const newArray = [name, ...drivers]
+  return newArray
+}
